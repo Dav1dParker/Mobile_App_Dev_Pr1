7e630d1503f431f2eb1333fa34edd8f55e853b0a
Andrey Chirva <112755473+Dav1dParker@users.noreply.github.com>
2023-02-14 22:16:30 +0300

    Created first toast

--- app/src/main/java/com/example/mobileappdevpr1/MainActivity.java
+++ app/src/main/java/com/example/mobileappdevpr1/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.mobileappdevpr1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
