0329a084724db85d7f27f0ae22a8fc2f11ea1d3c
Andrey Chirva <112755473+Dav1dParker@users.noreply.github.com>
2023-02-14 22:26:56 +0300

    Created toasts for each stage of the MainActivity lifecycle

--- app/src/main/java/com/example/mobileappdevpr1/MainActivity.java
+++ app/src/main/java/com/example/mobileappdevpr1/MainActivity.java
@@ -13,10 +13,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         Context context = getApplicationContext();
-        CharSequence text = "Hello toast!";
+        CharSequence text = "This is onCreate toast";
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Context context = getApplicationContext();
+        CharSequence text = "This is onStart toast";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Context context = getApplicationContext();
+        CharSequence text = "This is onStop toast";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Context context = getApplicationContext();
+        CharSequence text = "This is onDestroy toast";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Context context = getApplicationContext();
+        CharSequence text = "This is onPause toast";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Context context = getApplicationContext();
+        CharSequence text = "This is onResume toast";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
