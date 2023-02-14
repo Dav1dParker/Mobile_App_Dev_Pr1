7ffbba3bca44bc514629ca5ad6e4dc7b83c29fee
Andrey Chirva <112755473+Dav1dParker@users.noreply.github.com>
2023-02-14 22:18:13 +0300

    Created first toast

--- app/src/main/java/com/example/mobileappdevpr1/MainActivity.java
+++ app/src/main/java/com/example/mobileappdevpr1/MainActivity.java
@@ -17,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+
     }
 }
